c62f2b02909715672cde6ca2023c2f018bf26603
Darwin Rodriguez <darwinrv94@gmail.com>
2023-02-26 15:45:52 -0600

    page login

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html'
+})
+export class ButtonComponent {
+  @Input() typeBtn: 'button' | 'submit' | 'reset' = 'button';
+  @Input() color: string = 'primary';
+
+  get colors(): object {
+    return {
+      'bg-primary-700': this.color === 'primary',
+      'hover:bg-primary-800': this.color === 'primary',
+      'focus:bg-primary-300': this.color === 'primary',
+      'bg-success-700': this.color === 'success',
+      'hover:bg-success-800': this.color === 'success',
+      'focus:bg-success-300': this.color === 'success',
+      'bg-red-700': this.color === 'danger',
+      'hover:bg-red-800': this.color === 'danger',
+      'focus:bg-red-300': this.color === 'danger'
+    };
+  }
+}
